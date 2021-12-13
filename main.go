496473f2e121f0200819f7697e8834511e8d68d0
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2021-12-13 19:50:49 -0300

    change repo name

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	r := &github.RepositoryRelease{TagName: github.String("v0.1.0"), Name: github.String("teeest"), Body: github.String("generated from github client")}
 
-	release, _, err := client.Repositories.CreateRelease(context.Background(), "wesleimp-deprecated", "todo-gtk", r)
+	release, _, err := client.Repositories.CreateRelease(context.Background(), "wesleimp-deprecated", "labs", r)
 	if err != nil {
 		println("Ooooops", err.Error())
 		return
